aa3287907617218f7765b78790df0871ad46b0bf
WDD <2815582492@qq.com>
2024-08-02 11:39:37 +0800

    Add isPowerOfTwo.js

---
+++ Others/【231】isPowerOfTwo-2的幂.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  if (n == 0) return false
+  if (n == 1) return true
+  while (n % 2 == 0) {
+    n = n / 2
+  }
+  return n == 1 ? true : false
+};
