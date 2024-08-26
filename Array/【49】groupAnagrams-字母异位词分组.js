c9d2a892a590abf7979de6d26beb3d2ce04a73b5
WDD <2815582492@qq.com>
2024-08-26 09:48:09 +0800

    Add groupAnagrams 字母异位词分组

---
+++ Array/【49】groupAnagrams-字母异位词分组.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = new Map();
+  for (let str of strs) {
+    let array = Array.from(str);
+    array.sort();
+    let key = array.toString();
+    let list = map.get(key) ? map.get(key) : new Array();
+    list.push(str);
+    map.set(key, list);
+  }
+  return Array.from(map.values());
+};
+
+// 📘思路：排序比较
